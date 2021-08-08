3befb346bd0dfccd40605d00f71754dbb4892482
zy1o <a.ostrowski@outlook.com>
2021-08-08 18:07:56 +0200

    now actually added code

---
+++ src/doc_pytest_flow_chart/dummy_test_project/test_dummy.py
@@ -0,0 +1,30 @@
+""" This module contains a set of dummy tests which are
+supposed to trigger pytest hooks"""
+
+import pytest
+
+
+@pytest.fixture
+def fixture_yield():
+    print("setup fixture")
+    yield "yileding"
+    print("finalize fixture")
+
+
+@pytest.fixture():
+def fixture_finalizer(request):
+    def finalizer():
+        return "finalizer"
+    request.addfinalizer(finalizer)
+    return "actual fixture with explicit finalizer"
+
+@pytest.mark.parametrize("param1", range(5))
+def test_me_one(param1):
+    pass
+
+
+def test_fail_intentionally():
+    assert False
+
+
+
---
+++ src/doc_pytest_flow_chart/hookspec_generator.py
@@ -0,0 +1,43 @@
+import _pytest.hookspec as hs
+import inspect
+from typing import Union
+import pathlib
+
+def get_hooks() -> list:
+    """ Returns a list of pytest hooks"""
+    return [hook for hook in dir(hs) if hook.startswith("pytest_")]
+
+
+def get_hook_implementation(hook_name: str) -> str:
+    """ Returns string with code (implementation) for a given hook
+    name. Each implementation opens a text file and appends data
+    with information on the hook"""
+
+    hookspec_item = getattr(hs, hook_name, None)
+    if not hookspec_item:
+        return ""
+    args_dict = inspect.signature(hookspec_item).parameters
+
+    str_args = ", ".join([arg for arg in args_dict]).strip(",")
+    hook_impl_str = \
+        f"""
+@pytest.hookimpl()
+def {hook_name}({str_args}):
+    with open("hooks_order.txt", "a+") as hooks_file:
+        hooks_file.write("{hook_name}\\n")
+
+        """
+    return hook_impl_str
+
+
+def get_conftest_file() -> str:
+    """ Returns contents of a conftest.py with all hooks
+    implemented """
+    all_hooks = get_hooks()
+
+    conftest_content = "import pytest\n\n"
+    for hook in all_hooks:
+        conftest_content += get_hook_implementation(hook)
+    
+    return conftest_content
+
